e34947f4309d104ce7698fb2bb3f829ebdb32176
Justin <justin.smith5@icloud.com>
2017-02-19 12:42:14 -0500

    Added documentaiton

--- Set-CapsToControl.ps1
+++ Set-CapsToControl.ps1
@@ -1,3 +1,14 @@
-﻿$hexified = "00,00,00,00,00,00,00,00,02,00,00,00,1d,00,3a,00,00,00,00,00".Split(',') | % { "0x$_"}
+﻿<#
+    Set-CapsToControl.ps1
+    ---------------------
+
+    This script will change Windows 10 keyboard mapping Caps Lock to Control
+    
+    If you get an error message about "ExecutionPolicy" needing powershell files to be "RemoteSigned" do not
+    change it to something unsecure system wide like "Unrestricted" as many websites tell you. Just read/understand
+    this script and run an 'Unblock-File Set-CapsToControl.ps1' in Powershell to allow this file to run. 
+#>
+
+$hexified = "00,00,00,00,00,00,00,00,02,00,00,00,1d,00,3a,00,00,00,00,00".Split(',') | % { "0x$_"}
 $kbLayout = 'HKLM:\System\CurrentControlSet\Control\Keyboard Layout'
 New-ItemProperty -Path $kbLayout -Name "Scancode Map" -PropertyType Binary -Value ([byte[]]$hexified)
